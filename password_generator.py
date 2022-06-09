2e6bc4dff2d2f075953360f53c19daf4115b4c63
dineshsuthar03 <104675302+dineshsuthar03@users.noreply.github.com>
2022-06-09 23:47:46 +0530

    Add files via upload

---
+++ password_generator.py
@@ -0,0 +1,28 @@
+import random
+
+uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lowercase_letters = uppercase_letters.lower()
+digits = "0123456789"
+symbols = "-+*#@"
+
+upper,lower,nums,syms = True, True,True,False
+
+seed1 ="dinesh"
+random.seed(seed1)
+
+all = ""
+if upper:
+    all += uppercase_letters
+if lower:
+    all += lowercase_letters
+if nums:
+    all += digits
+if syms:
+    all += symbols
+
+length = 8
+amount =10
+
+for x in range(amount):
+    password = "".join(random.sample(all,length))
+    print(password)
